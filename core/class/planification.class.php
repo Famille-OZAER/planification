ce492f43c1f5bca9b73cee18868945208fce4d36
Mike <michaer@ozaer.eu>
2020-11-29 10:38:04 +0100

    test

--- core/class/planification.class.php
+++ core/class/planification.class.php
@@ -30,6 +30,9 @@ class planification extends eqLogic {
 		}else{
 			if(date_create_from_format("Y-m-d H:i:s",$cron->getNextRunDate())->getTimestamp()>time()){return;}
 		}
+
+
+
 		
 		$eqLogic = self::byId($_option['eqLogic_Id']);
 		log::add('planification', 'debug', "pull de : " . $eqLogic->getName() );
