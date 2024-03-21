51e9477ad2271f9562788bfdd66b3b6065ac4f30
Mike <michael@ozaer.eu>
2024-03-21 21:05:02 +0100

    test

--- plugin_info/install.php
+++ plugin_info/install.php
@@ -53,7 +53,7 @@ function planification_update() {
 			$planifications_new .='}]';
 			$nom_fichier_source = dirname(__FILE__) ."/../../planifications/" . $eqLogic->getId() . ".json"; 
 			$nom_fichier_cible =  dirname(__FILE__) ."/../../planifications/" . $eqLogic->getId() . "_old.json"; 
-			planification::add_log('debug',"ok",$planifications_new);  
+			planification::add_log('debug',$planifications_new);  
 			
 			//copy( $nom_fichier_source , $nom_fichier_cible);
 			//$fichier = fopen($nom_fichier_source, 'w');
