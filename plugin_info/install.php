438a4e16512d60420cbed49628a917a48615efb9
Mike <michael@ozaer.eu>
2024-04-19 11:39:48 +0200

    test intall

--- plugin_info/install.php
+++ plugin_info/install.php
@@ -20,7 +20,7 @@ function find_all_files($dir){
 			continue;
 		}
 
-        foreach(find_all_files($dir) as $value){
+        foreach(find_all_files($dir.$value) as $value){
 		log::add('planification_update', 'debug', $value);
             $result[]=$value;
         }
